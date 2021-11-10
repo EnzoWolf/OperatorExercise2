9204e77e8d46c59242296ca3531bd09adf589c41
EnzoWolf <allen@enzowolf.com>
2021-11-10 18:13:06 -0500

    Operator Exercise 2

---
+++ OperatorExcercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExcercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get radius of circle from user
+            Console.WriteLine("What is the radius of your circle:");
+            var radius = double.Parse(Console.ReadLine());
+
+            //Call AreaOfCircle method
+            var areaTotal = AreaOfCircle(radius);
+
+            //Output the area of the circle
+            Console.WriteLine($"\r\nThe area of a circle with the radius of {radius} is {areaTotal}.");
+
+            Console.ReadKey();
+
+        }
+
+        //Method to calculate the area of a circle
+        public static double AreaOfCircle(double radius) //AreaOfCircle was declared in the C# Operators Excercise document
+        {
+            var circleArea = Math.PI * (radius * radius); //Formula for calculating the area of a circle 
+            return circleArea;
+        }
+    }
+}
